00a69688aac8bdff005e129ea0a0b81c8bf4075e
Paige LaBarr <paige.labarr@flcc.edu>
2026-09-02 15:29:34 +0000

    added practice files

---
+++ 00-first-week/practice1.py
@@ -0,0 +1,11 @@
+# print a greeting
+print("Welcome to Paige's Pizza Shop 🍕")
+
+# ask a question - collect input and store
+topping = input("What topping do you want on your pizza? ")
+
+# print a statement that includes the response
+# f - formatted string - variable in your print statement
+print(f"Ok, you'll get {topping} on your pizza.")
+print("Ok, you'll get", topping, "on your pizza.")
+print("Ok, you'll get " + topping + " on your pizza.")
